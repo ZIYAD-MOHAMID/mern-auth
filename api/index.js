1fbfde969fb2b85f3a0ee07054a829b3f7a44805
Your Name <ziyadsha44@gmail.com>
2024-11-13 16:26:20 +0200

    create and run backend

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () =>
+  console.log(
+    ` Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
