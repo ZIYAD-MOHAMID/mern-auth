6ae461cc59ec6a75aba2c06e415d0803a8cbcc10
Your Name <ziyadsha44@gmail.com>
2024-11-14 03:25:14 +0200

    Profile Page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
-const router = express.Router();
+import { updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
-router.get("/text", test);
+const router = express.Router();
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
